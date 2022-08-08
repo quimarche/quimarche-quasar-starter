345666309ad28713037c4968c724400b44b8c8bb
quimarche <chris@quimarche.co.uk>
2022-08-08 08:05:12 +0100

    4 files modified, 1 added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import { Quasar } from 'quasar'
 import { createApp } from 'vue'
+
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/roboto-font/roboto-font.css'
+
+import 'quasar/src/css/index.sass'
 
 import App from '@/Components/App.vue'
 
-createApp(App).mount('#app')
+createApp(App).myApp.use(Quasar, { plugins: {} }).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import eslintPlugin from 'vite-plugin-eslint'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [eslintPlugin(), vue()],
+  plugins: [
+    eslintPlugin(),
+    quasar({ sassVariables: 'src/quasar-variables.sass' }),
+    vue({ template: { transformAssetUrls } })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
